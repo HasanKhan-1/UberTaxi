99b5d567ccdb1eb494654808deb003cfdd296586
Hasan <hasanmahmkhan@gmail.com>
2025-03-19 19:02:58 -0400

    old code works in motor_test

--- motor_test.py
+++ motor_test.py
@@ -1,53 +1,131 @@
-from gpiozero import PWMOutputDevice, DigitalOutputDevice
-from time import sleep
+# from gpiozero import PWMOutputDevice, DigitalOutputDevice
+# from time import sleep
 
-# Motor Pins
-IN1 = DigitalOutputDevice(17)
-IN2 = DigitalOutputDevice(27)
-IN3 = DigitalOutputDevice(22)
-IN4 = DigitalOutputDevice(23)
-ENA = PWMOutputDevice(24)  # Speed control (PWM)
-ENB = PWMOutputDevice(25)  # Speed control (PWM)
+# # Motor Pins
+# IN1 = DigitalOutputDevice(17)
+# IN2 = DigitalOutputDevice(27)
+# IN3 = DigitalOutputDevice(22)
+# IN4 = DigitalOutputDevice(23)
+# ENA = PWMOutputDevice(24)  # Speed control (PWM)
+# ENB = PWMOutputDevice(25)  # Speed control (PWM)
 
-ENAb = PWMOutputDevice(6)  # Speed control (PWM)
-ENBb = PWMOutputDevice(5)  # Speed control (PWM)
+# ENAb = PWMOutputDevice(6)  # Speed control (PWM)
+# ENBb = PWMOutputDevice(5)  # Speed control (PWM)
 
-# Setup PWM for speed control
-ENA.value = 0.5  # 50% speed
-ENB.value = 0.5
-ENAb.value = 0.5  # 50% speed
-ENBb.value = 0.5
+# # Setup PWM for speed control
+# ENA.value = 0.5  # 50% speed
+# ENB.value = 0.5
+# ENAb.value = 0.5  # 50% speed
+# ENBb.value = 0.5
 
 
-# Move both motors forward
-IN1.on()
-IN2.off()
-IN3.on()
-IN4.off()
+# # Move both motors forward
+# IN1.on()
+# IN2.off()
+# IN3.on()
+# IN4.off()
 
-sleep(0.5)
+# sleep(0.5)
 
-# Stop motors
-IN1.off()
-IN2.off()
-IN3.off()
-IN4.off()
+# # Stop motors
+# IN1.off()
+# IN2.off()
+# IN3.off()
+# IN4.off()
 
-sleep(0.5)
+# sleep(0.5)
 
-# Move both motors backward
-IN1.off()
-IN2.on()
-IN3.off()
-IN4.on()
+# # Move both motors backward
+# IN1.off()
+# IN2.on()
+# IN3.off()
+# IN4.on()
 
-sleep(0.5)
+# sleep(0.5)
 
-# Stop motors
-IN1.off()
-IN2.off()
-IN3.off()
-IN4.off()
+# # Stop motors
+# IN1.off()
+# IN2.off()
+# IN3.off()
+# IN4.off()
 
-ENA.off()
-ENB.off()
+# ENA.off()
+# ENB.off()
+
+
+import cv2
+import numpy as np
+from gpiozero import PWMOutputDevice, DigitalOutputDevice, Motor 
+cap = cv2.VideoCapture(0)
+cap.set(3, 160)
+cap.set(4, 120)
+in1 = 17
+# in2 used to be gpio 18
+in2 = 26
+in3 = 22
+#in4 used to be gpio 23
+in4 = 16
+
+en1 = 24 # for forwards for speed
+en2 = 25 # for forwards for speed
+en1b = 19 # backwards for speed
+en2b = 5 # backwards for speed
+
+p1 = PWMOutputDevice(en1, frequency=100)
+p2 = PWMOutputDevice(en1b, frequency=100)
+p1.value = 1
+p2.value = 1
+
+p3 = PWMOutputDevice(en2, frequency=100)
+p4 = PWMOutputDevice(en2b, frequency=100)
+p3.value = 1
+p4.value = 1
+
+# left_forward = DigitalOutputDevice(in1)
+# left_backward = DigitalOutputDevice(in2)
+
+left_forward = DigitalOutputDevice(in1)
+left_backward = DigitalOutputDevice(in2)
+right_forward = DigitalOutputDevice(in3)
+right_backward= DigitalOutputDevice(in4)
+
+
+left_forward.off()
+left_backward.off()
+right_backward.off()
+right_forward.off()
+
+while True:
+    ret, frame = cap.read()
+    low_b = np.uint8([150,150,150])
+    high_b = np.uint8([250, 250, 250])
+    mask = cv2.inRange(frame, low_b, high_b )
+    contours, hierarchy = cv2.findContours(mask, 1, cv2.CHAIN_APPROX_NONE)
+    if len(contours) > 0 :
+        c = max(contours, key=cv2.contourArea)
+        M = cv2.moments(c)
+        if M["m00"] !=0 :
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+            print("CX : "+str(cx)+"  CY : "+str(cy))
+
+            if cx <120 and cx > 40 :
+                print("Straight, on track")
+            
+                left_backward.on()
+        
+        cv2.circle(frame, (cx,cy), 5, (255,255,255), -1)
+        cv2.drawContours(frame, c, -1, (0,255,0), 1)
+                    
+
+    else :
+        print("I don't see the line")
+     
+    cv2.imshow("Mask",mask)
+    cv2.imshow("Frame",frame)
+    if cv2.waitKey(1) & 0xff == ord('q'):   # 1 is the time in ms
+   
+        break
+cap.release()
+cv2.destroyAllWindows()
+
--- test_m.py
+++ test_m.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-from gpiozero import PWMOutputDevice, DigitalOutputDevice, Motor 
+from gpiozero import PWMOutputDevice, DigitalOutputDevice
 cap = cv2.VideoCapture(0)
 cap.set(3, 160)
 cap.set(4, 120)
@@ -8,7 +8,8 @@ in1 = 17
 # in2 used to be gpio 18
 in2 = 27
 in3 = 22
-in4 = 23
+#in4 used to be gpio 23
+in4 = 16
 
 en1 = 24 # for forwards for speed
 en2 = 25 # for forwards for speed
@@ -41,8 +42,8 @@ right_forward.off()
 
 while True:
     ret, frame = cap.read()
-    low_b = np.uint8([150,150,150])
-    high_b = np.uint8([250, 250, 250])
+    low_b = np.uint8([250, 0, 0])
+    high_b = np.uint8([255, 77, 77])
     mask = cv2.inRange(frame, low_b, high_b )
     contours, hierarchy = cv2.findContours(mask, 1, cv2.CHAIN_APPROX_NONE)
     if len(contours) > 0 :
@@ -52,64 +53,62 @@ while True:
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
             print("CX : "+str(cx)+"  CY : "+str(cy))
+
             if cx >= 120 :
                 print("Turn Left")
-                # in1_pin.on()
-                # in2_pin.off()
-                # in3_pin.off()
-                # in4_pin.on()
-                # speed = 0.5
                 
                 left_forward.off()
-                left_backward.on()
-                p1.value = 0.5
-                p4.value = 0.5
-                p2.value = 0.5
-                
-                right_backward.off()
-                right_forward.on()
-                p3.value = 0.5 
-            
+                left_backward.on() #2
+                right_forward.off()
+                right_backward.on() #4
+
+                p4.value = 0.4 # right backwards speed 
+                p2.value = 0.2 # left backward speed
+
+                p1.value = 0.2 # right forward speed
+                p3.value = 0.2 # left forward speed
+
             if cx <120 and cx > 40 :
                 print("Straight, on track")
                 
-                left_forward.off()
-                left_backward.on()
-                p1.value = 0.5
-                p4.value = 0.5
-                p2.value = 0.5
-                
-                right_backward.off()
-                right_forward.on()
-                p3.value = 0.5                 
+                left_forward.on() 
+                left_backward.off()
+
+                right_forward.off()
+                right_backward.on()
+
+                p1.value = 0.4
+                p4.value = 0.4
+                p2.value = 0.4               
+                p3.value = 0.4
+                              
 
             if cx <=40 :
                 print("Turn Right")
                 left_forward.off()
                 left_backward.on() #2
-                right_backward.on() #4
                 right_forward.off()
+                right_backward.on() #4
 
-                p4.value = 0.5 
-                p2.value = 1
+                p4.value = 0.2 # right backwards speed 
+                p2.value = 0.4 # left backward speed
 
-            cv2.circle(frame, (cx,cy), 5, (255,255,255), -1)
-            cv2.drawContours(frame, c, -1, (0,255,0), 1)
+                p1.value = 0.4 # right forward speed
+                p3.value = 0.4 # left forward speed
 
+        cv2.circle(frame, (cx,cy), 5, (255,255,255), -1)
+        cv2.drawContours(frame, c, -1, (0,255,0), 1)
+    
     else :
         print("I don't see the line")
         left_forward.off()
         left_backward.off()
         right_backward.off()
         right_forward.off()
+
     cv2.imshow("Mask",mask)
     cv2.imshow("Frame",frame)
-    if cv2.waitKey(1) & 0xff == ord('q'):   # 1 is the time in ms
-        left_forward.off()
-        left_backward.off()
-        right_backward.off()
-        right_forward.off()
-        break
+
 cap.release()
 cv2.destroyAllWindows()
 
