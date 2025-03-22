3aa419ea6c431c467ec425a2386f010a48376d96
MaureenSun <nemo@gaussmath.ca>
2025-03-22 15:28:54 -0400

    servo test, basic

--- server_test.py
+++ server_test.py
@@ -1,14 +1,12 @@
-from gpiozero import AngularServo
+from gpiozero import Servo
 from time import sleep
 
-servo = AngularServo(14, min_angle=0, max_angle = 180, min_pulse_width=0.5/1000, max_pulse_width=2.4/1000)
+servo = Servo(17)
 
-def set_angle(angle):
-    servo.angle = angle
-    sleep(1)
-
-try:
-    while True:
-        set_angle(angle=90)
-except KeyboardInterrupt:
-    print("servo stopped")
+while True:
+    servo.min()
+    sleep(2)
+    servo.mid()
+    sleep(2)
+    servo.max()
+    sleep(2)
