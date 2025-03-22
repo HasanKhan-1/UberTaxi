65efdad833dcfd31082d7e6dd5ccfd8df229c5d2
Hasan <hasanmahmkhan@gmail.com>
2025-03-22 14:18:53 -0400

    pi5 fw/bw wrks

--- only_motors_pi5_test.py
+++ only_motors_pi5_test.py
@@ -1,65 +1,70 @@
-from gpiozero import OutputDevice, PWMOutputDevice
-import cv2
-import numpy as np
-import time
+from gpiozero import PWMOutputDevice, DigitalOutputDevice
+from time import sleep
 
-# Motor control pins
-in1 = 6
-in2 = 5
-in3 = 25
-in4 = 24
+# Motor Pins
+IN1 = DigitalOutputDevice(6)
+IN2 = DigitalOutputDevice(5)
+IN3 = DigitalOutputDevice(25)
+IN4 = DigitalOutputDevice(24)
+ENA = PWMOutputDevice(17)  # Speed control (PWM)
+ENB = PWMOutputDevice(22)  # Speed control (PWM)
 
-# PWM speed control pins
-ena_top = 26   # Forward speed control
-ena_bottom = 16  # Forward speed control
-enb_top = 23  # Backward speed control
-enb_bottom = 22  # Backward speed control
+ENAb = PWMOutputDevice(27)  # Speed control (PWM)
+ENBb = PWMOutputDevice(23)  # Speed control (PWM)
 
-# Initialize motor control pins
-motor_in1 = OutputDevice(in1)
-motor_in2 = OutputDevice(in2)
-motor_in3 = OutputDevice(in3)
-motor_in4 = OutputDevice(in4)
+# Setup PWM for speed control
+ENA.value = 0.5  # 50% speed
+ENB.value = 0.5
+ENAb.value = 0.5  # 50% speed
+ENBb.value = 0.5
 
-# Initialize PWM for speed control
-pwm_ena_top = PWMOutputDevice(ena_top, initial_value=0)
-pwm_ena_bottom = PWMOutputDevice(ena_bottom, initial_value=0)
-pwm_enb_top = PWMOutputDevice(enb_top, initial_value=0)
-pwm_enb_bottom = PWMOutputDevice(enb_bottom, initial_value=0)
+
+def move_forward():
+    """Move both motors forward."""
+    print("Moving forward")
+    IN1.on()
+    IN2.off()
+    IN3.on()
+    IN4.off()
+    ENA.value = 0.5
+    ENB.value = 0.5
+    ENAb.value = 0.5
+    ENBb.value = 0.5
+
+def move_backward():
+    """Move both motors backward."""
+    print("Moving backward")
+    IN1.off()
+    IN2.on()
+    IN3.off()
+    IN4.on()
+    ENA.value = 0.5
+    ENB.value = 0.5
+    ENAb.value = 0.5
+    ENBb.value = 0.5
 
 def stop_motors():
-    motor_in1.off()
-    motor_in2.off()
-    motor_in3.off()
-    motor_in4.off()
-    pwm_ena_top.value = 0
-    pwm_ena_bottom.value = 0
-    pwm_enb_top.value = 0
-    pwm_enb_bottom.value = 0
-
-def move_forward(speed):
-    motor_in1.on()
-    motor_in2.off()
-    motor_in3.on()
-    motor_in4.off()
-    pwm_ena_top.value = speed 
-    pwm_ena_bottom.value = speed 
-
-def move_spin(speed):
-    motor_in1.off()
-    motor_in2.on()
-    motor_in3.on()
-    motor_in4.off()
-    pwm_ena_top.value = speed / 100
-    pwm_ena_bottom.value = speed / 100
-
-def move_backwards(speed):
-    motor_in1.off()
-    motor_in2.on()
-    motor_in3.off()
-    motor_in4.on()
-    pwm_enb_top.value = speed / 100
-    pwm_enb_bottom.value = speed / 100
+    """Stop both motors."""
+    print("Stopping motors")
+    IN1.off()
+    IN2.off()
+    IN3.off()
+    IN4.off()
+    ENA.off()
+    ENB.off()
+    ENAb.off()
+    ENBb.off()
 
 if __name__ == "__main__":
-        move_forward(1)  # Move forward with 100% speed
+    while True:
+        move_forward()
+        sleep(2)  # Move forward for 2 seconds
+        stop_motors()
+        sleep(1)  # Stop for 1 second
+        move_backward()
+        sleep(2)  # Move backward for 2 seconds
+        stop_motors()
+    # except KeyboardInterrupt:
+    #     pass
+    # finally:
+    #     stop_motors()
