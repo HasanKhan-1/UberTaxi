bc2e822d8e36bf0ae663600a028693ca2a1d9ab8
MaureenSun <nemo@gaussmath.ca>
2025-03-22 12:26:05 -0400

    changed pins

--- only_motors_pi5_test.py
+++ only_motors_pi5_test.py
@@ -4,14 +4,14 @@ import numpy as np
 import time
 
 # Motor control pins
-in1 = 6
-in2 = 5
-in3 = 25
-in4 = 24
+in1 = 26
+in2 = 16
+in3 = 6
+in4 = 5
 
 # PWM speed control pins
-ena_top = 26   # Forward speed control
-ena_bottom = 16  # Forward speed control
+ena_top = 25   # Forward speed control
+ena_bottom = 24  # Forward speed control
 enb_top = 23  # Backward speed control
 enb_bottom = 22  # Backward speed control
 
@@ -38,28 +38,28 @@ def stop_motors():
     pwm_enb_bottom.value = 0
 
 def move_forward(speed):
+    motor_in1.on()
+    motor_in2.off()
+    motor_in3.on()
+    motor_in4.off()
+    pwm_ena_top.value = speed / 100
+    pwm_ena_bottom.value = speed / 100
+
+def move_spin(speed):
+    motor_in1.off()
+    motor_in2.on()
+    motor_in3.on()
+    motor_in4.off()
+    pwm_ena_top.value = speed / 100
+    pwm_ena_bottom.value = speed / 100
+
+def move_backwards(speed):
     motor_in1.off()
     motor_in2.on()
     motor_in3.off()
     motor_in4.on()
-    pwm_ena_top.value = speed 
-    pwm_ena_bottom.value = speed
-
-# def move_spin(speed):
-#     motor_in1.off()
-#     motor_in2.on()
-#     motor_in3.on()
-#     motor_in4.off()
-#     pwm_ena_top.value = speed / 100
-#     pwm_ena_bottom.value = speed / 100
-
-# def move_backwards(speed):
-#     motor_in1.off()
-#     motor_in2.on()
-#     motor_in3.off()
-#     motor_in4.on()
-#     pwm_enb_top.value = speed / 100
-#     pwm_enb_bottom.value = speed / 100
+    pwm_enb_top.value = speed / 100
+    pwm_enb_bottom.value = speed / 100
 
 if __name__ == "__main__":
         stop_motors()
