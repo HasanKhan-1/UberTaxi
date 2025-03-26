22df64ae0b1cc51c3562f3d66c4d066c333fe3ab
Hasan <hasanmahmkhan@gmail.com>
2025-03-26 16:04:02 -0400

    FIX: right enc vals

--- src/motors.py
+++ src/motors.py
@@ -9,14 +9,14 @@ class MotorController:
     def __init__(self):
         """Initialize motor pins and PWM."""
         # Motor Pins
-        self.IN1 = DigitalOutputDevice(6)
+        self.IN1 = DigitalOutputDevice(14)
         self.IN2 = DigitalOutputDevice(5)
-        self.IN3 = DigitalOutputDevice(25)
-        self.IN4 = DigitalOutputDevice(24)
-        self.ENA = PWMOutputDevice(17)  # Speed control (PWM)
-        self.ENB = PWMOutputDevice(22)  # Speed control (PWM)
+        self.IN3 = DigitalOutputDevice(2)
+        self.IN4 = DigitalOutputDevice(3)
+        self.ENA = PWMOutputDevice(4)  # Speed control (PWM)
+        self.ENB = PWMOutputDevice(23)  # Speed control (PWM)
         self.ENAb = PWMOutputDevice(27)  # Speed control (PWM)
-        self.ENBb = PWMOutputDevice(23)  # Speed control (PWM)
+        self.ENBb = PWMOutputDevice(24)  # Speed control (PWM)
 
         # Set initial speed to 50%
         self.ENA.value = 0.5
