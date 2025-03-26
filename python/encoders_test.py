2d58953791f34b90ee62aa3968bbef699103e8ab
MaureenSun <nemo@gaussmath.ca>
2025-03-25 23:00:47 -0400

    new encoder troubleshooting

--- python/encoders_test.py
+++ python/encoders_test.py
@@ -1,66 +1,3 @@
-import RPi.GPIO as GPIO
-import time
-
-# Encoder 1 (Left) GPIO Pins
-encoder1PinA = 10  # GPIO Pin 17 (Physical Pin 11)
-encoder1PinB = 9  # GPIO Pin 27 (Physical Pin 13)
-
-# Encoder 2 (Right) GPIO Pins
-encoder2PinA = 21  # GPIO Pin 22 (Physical Pin 15)
-encoder2PinB = 18  # GPIO Pin 23 (Physical Pin 16)
-
-# Initialize encoder positions
-encoderPos1 = 0
-encoderPos2 = 0
-
-# Setup the GPIO mode
-GPIO.setmode(GPIO.BCM)  # Use Broadcom GPIO pin numbering
-GPIO.setwarnings(False)  # Disable GPIO warnings
-
-# Set up Encoder 1 pins
-GPIO.setup(encoder1PinA, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(encoder1PinB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-# Set up Encoder 2 pins
-GPIO.setup(encoder2PinA, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(encoder2PinB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-# Define the callback functions for interrupts
-def handle_encoder1A(channel):
-    """Callback function for Encoder 1."""
-    global encoderPos1
-    if GPIO.input(encoder1PinA) == GPIO.input(encoder1PinB):
-        encoderPos1 += 1
-    else:
-        encoderPos1 -= 1
-
-def handle_encoder2A(channel):
-    """Callback function for Encoder 2."""
-    global encoderPos2
-    if GPIO.input(encoder2PinA) == GPIO.input(encoder2PinB):
-        encoderPos2 += 1
-    else:
-        encoderPos2 -= 1
-
-# Set up interrupts for both encoders
-GPIO.add_event_detect(encoder1PinA, GPIO.BOTH, callback=handle_encoder1A, bouncetime=1)
-GPIO.add_event_detect(encoder2PinA, GPIO.BOTH, callback=handle_encoder2A, bouncetime=1)
-
-try:
-    while True:
-        # Print the encoder positions, scaling them
-        print("Encoder 1 (Left): {:.2f} | Encoder 2 (Right): {:.2f}".format(
-            encoderPos1 * (210.48666 / (12 * 34 * 2.36)),
-            encoderPos2 * (210.48666 / (12 * 34 * 2.36))
-        ))
-        time.sleep(0.1)
-
-except KeyboardInterrupt:
-    print("Program interrupted")
-
-finally:
-    GPIO.cleanup()  # Clean up GPIO settings
-
 
 # from gpiozero import Button
 # from signal import pause
@@ -99,3 +36,56 @@ finally:
 
 # except KeyboardInterrupt:
 #     print("Program interrupted")
+
+from gpiozero import Button
+from time import sleep
+
+# Encoder 1 (Left) GPIO Pins
+encoder1PinA = 10  # GPIO17
+encoder1PinB = 9  # GPIO22
+
+# Encoder 2 (Right) GPIO Pins
+encoder2PinA = 21   # GPIO7
+encoder2PinB = 18  # GPIO1
+
+# Initialize encoder positions
+encoderPos1 = 0
+encoderPos2 = 0
+
+# Define callback functions for encoders
+def handle_encoder1A():
+    global encoderPos1
+    if encoder1B.is_pressed:  # If B is HIGH when A changes, moving one direction
+        encoderPos1 += 1
+    else:  # If B is LOW when A changes, moving the other direction
+        encoderPos1 -= 1
+
+def handle_encoder2A():
+    global encoderPos2
+    if encoder2B.is_pressed:
+        encoderPos2 += 1
+    else:
+        encoderPos2 -= 1
+
+# Setup gpiozero Buttons with pull-ups and debouncing
+encoder1A = Button(encoder1PinA, pull_up=True, bounce_time=0.001)
+encoder1B = Button(encoder1PinB, pull_up=True, bounce_time=0.001)
+
+encoder2A = Button(encoder2PinA, pull_up=True, bounce_time=0.001)
+encoder2B = Button(encoder2PinB, pull_up=True, bounce_time=0.001)
+
+# Attach the event handlers **only to channel A**
+encoder1A.when_pressed = handle_encoder1A  # Trigger only on A signal changes
+encoder2A.when_pressed = handle_encoder2A
+
+# Main loop to print encoder values
+try:
+    while True:
+        print("Encoder 1 (Left): {:.2f} | Encoder 2 (Right): {:.2f}".format(
+            encoderPos1 * (210.48666 / (12 * 34 * 2.36)),
+            encoderPos2 * (210.48666 / (12 * 34 * 2.36))
+        ))
+        sleep(0.1)
+
+except KeyboardInterrupt:
+    print("Program interrupted")
