c98cbbdea1760868402a2f13186c4998499eecbf
Hasan <hasanmahmkhan@gmail.com>
2025-03-26 16:04:44 -0400

    switch

--- src/motors.py
+++ src/motors.py
@@ -11,8 +11,8 @@ class MotorController:
         # Motor Pins
         self.IN1 = DigitalOutputDevice(14)
         self.IN2 = DigitalOutputDevice(5)
-        self.IN3 = DigitalOutputDevice(2)
-        self.IN4 = DigitalOutputDevice(3)
+        self.IN3 = DigitalOutputDevice(3)
+        self.IN4 = DigitalOutputDevice(2)
         self.ENA = PWMOutputDevice(4)  # Speed control (PWM)
         self.ENB = PWMOutputDevice(23)  # Speed control (PWM)
         self.ENAb = PWMOutputDevice(27)  # Speed control (PWM)
